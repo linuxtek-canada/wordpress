417741da07d430baa29dd0965230f312b5441204
Jason Paul <jasonpa@gmail.com>
2021-11-18 22:23:35 -0500

    11/18 Updates

---
+++ Terraform/cloudsql.tf
@@ -0,0 +1,53 @@
+resource "google_compute_network" "private_network" {
+  provider = google-beta
+
+  name = "private-network"
+}
+
+resource "google_compute_global_address" "private_ip_address" {
+  provider = google-beta
+
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.private_network.id
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  provider = google-beta
+
+  network                 = google_compute_network.private_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
+resource "google_sql_database_instance" "instance" {
+  provider = google-beta
+
+  name             = "wordpress-${random_id.db_name_suffix.hex}"
+  region           = "northamerica-northeast2"
+  database_version = "MYSQL_5_7"
+  deletion_protection = false
+  
+  depends_on = [google_service_networking_connection.private_vpc_connection]
+
+  settings {
+    tier = "db-f1-micro"
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.private_network.id
+    }
+  }
+}
+
+provider "google-beta" {
+  credentials = file("sacred-attic-332602-8d834239a53e.json")
+  project     = "sacred-attic-332602"
+  region      = "northamerica-northeast2"
+  zone        = "northamerica-northeast2-a"
+}
---
+++ Terraform/main.tf
@@ -0,0 +1,8 @@
+// Configure the Google Cloud provider
+provider "google" {
+ credentials = file("sacred-attic-332602-8d834239a53e.json")
+ project     = "sacred-attic-332602"
+ region      = "northamerica-northeast2"
+}
+
+
